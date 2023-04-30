812fef228177e4f2b4610b940dbabfaa8521fd12
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-30 16:59:18 -0300

    add navbar

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  private roles: string[] = [];
+  isLoggedIn: boolean = false;
+  isModerator: boolean = false;
+  isAdmin: boolean = false;
+  username?: string;
 
+  constructor(
+    private authService: AuthService,
+    private storage: StorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storage.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storage.getUser();
+
+      this.roles = user.roles;
+      this.isModerator = this.roles.includes('ROLE_MODERATOR');
+      this.isAdmin = this.roles.includes('ROLE_ADMIN');
+
+      this.username = user.username;
+    }
+  }
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: (resData) => {
+        this.storage.clean();
+
+        window.location.reload();
+      },
+      error: (errRes) => {
+        console.log(errRes);
+      },
+    });
+  }
 }
