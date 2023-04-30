28edee893b91610be863fedbcbdef47ade0bc803
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-30 11:30:25 -0300

    implement user data service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const API_URL = 'http://localhost:8080/api/test';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPublicCOntent(): Observable<any> {
+    return this.getData('all');
+  }
+
+  getUserBoard(): Observable<any> {
+    return this.getData('user');
+  }
+
+  getModeratorBoard(): Observable<any> {
+    return this.getData('mod');
+  }
+
+  getAdminBoard(): Observable<any> {
+    return this.getData('admin');
+  }
+
+  private getData(boardName: string) {
+    const endpoint = API_URL + boardName;
+
+    return this.http.get(endpoint, { responseType: 'text' });
+  }
 }
