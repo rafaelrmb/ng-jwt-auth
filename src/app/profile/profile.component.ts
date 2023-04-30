432a612a99fad046c9b9480c91fe3ca6e2970a25
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-30 15:51:42 -0300

    add profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { UserService } from './../services/user.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  currentUser: any;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.userService.getUserBoard();
+  }
 }
