74b9ac221c7a693b831fd7f8ec8a413fe5d95bd3
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-30 15:57:08 -0300

    add home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { UserService } from './../services/user.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  content?: string;
 
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userService.getPublicContent().subscribe({
+      next: (resData) => {
+        this.content = resData;
+      },
+      error: (errRes) => {
+        console.log(errRes);
+        if (errRes) {
+          this.content = errRes.message;
+        } else {
+          this.content = 'Error Status' + errRes.status;
+        }
+      },
+    });
+  }
 }
