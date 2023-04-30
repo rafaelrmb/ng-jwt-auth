475435c5e22d4344f5890b0c475bc9bf55ea8282
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-30 12:16:59 -0300

    add register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm: any = {
+    username: null,
+    email: null,
+    password: null,
+  };
+  isSuccessful: boolean = false;
+  didSignUpFail: boolean = false;
+  errorMsg = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit(): void {
+    const { username, email, password } = this.registerForm;
+
+    this.authService.register(username, email, password).subscribe({
+      next: (resData) => {
+        this.isSuccessful = true;
+      },
+      error: (errRes) => {
+        this.errorMsg = errRes.error.message;
+        this.didSignUpFail = true;
+      },
+    });
+  }
+}
