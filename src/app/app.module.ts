39f0e85e94f9d7180996c44133978bdd32d37c23
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-30 10:59:33 -0300

    fix typo on register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { ResgiterComponent } from './resgiter/resgiter.component';
+import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
@@ -15,18 +15,15 @@ import { BoardUserComponent } from './board-user/board-user.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    ResgiterComponent,
+    RegisterComponent,
     HomeComponent,
     ProfileComponent,
     BoardAdminComponent,
     BoardModeratorComponent,
-    BoardUserComponent
+    BoardUserComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
